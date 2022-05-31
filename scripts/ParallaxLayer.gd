cda890e82c40d5d3f68d3739fbf58590662e8ee7
Interroblank <coronyt.apoelius@gmail.com>
2022-05-30 20:08:26 -0500

    Rigged Stage 4 with parallax background and new lighting.

---
+++ scripts/ParallaxLayer.gd
@@ -0,0 +1,10 @@
+extends Sprite
+
+export(float) var speed = 0
+export(int) var orig_x = 0
+export(int) var orig_y = 0
+
+func _physics_process(_delta):
+	self.translate(Vector2(-speed, 0))
+	if(self.global_transform.origin.x <= -1440):
+		self.global_transform.origin = Vector2(orig_x, orig_y)
