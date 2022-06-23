1b493cd8c435cfc8dd416d88572a22e670db44a4
Interroblank <coronyt.apoelius@gmail.com>
2022-06-23 00:25:34 -0500

    Rigged Stages 1, 2, 3, and 5 with parallax backgrounds and new lighting.

--- scripts/RailCanvas.gd
+++ scripts/RailCanvas.gd
@@ -6,12 +6,12 @@ func rise_seq():
 
 func _on_RailRise_animation_finished(anim_name):
 	if anim_name == "rise_1":
-		$RailRise.play("rise_2")
+		# $RailRise.play("rise_2")
 		SoundManager.play_score2()
-		SoundManager.play_title3()
-	if anim_name == "rise_2":
-		$RailRise.play("rise_3")
-		SoundManager.play_title3()
-	if anim_name == "rise_3":
-		SoundManager.play_score1()
 		SoundManager.stop_title3()
+	# if anim_name == "rise_2":
+		# $RailRise.play("rise_3")
+		# SoundManager.play_title3()
+	# if anim_name == "rise_3":
+		# SoundManager.play_score1()
+		# SoundManager.stop_title3()
