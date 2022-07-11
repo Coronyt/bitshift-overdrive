70ea161bf4f134cef69e2076b79fdc1bb6930489
Interroblank <coronyt.apoelius@gmail.com>
2022-07-10 20:08:42 -0500

    Implemented audio visualizer across all active Stages.

---
+++ scripts/Visualizer.gd
@@ -0,0 +1,34 @@
+extends Node2D
+
+const FREQ_MAX = 11050.0
+const MIN_DB = 60
+
+const VU_COUNT = 19
+const WIDTH = (1280 - 64)
+const HEIGHT = 300
+
+var res_w = 1280
+var aud_spectrum
+
+func _draw():
+	var w = WIDTH / VU_COUNT
+	var prev_hz = 0
+	for i in range(0, VU_COUNT):
+		var hz = i * FREQ_MAX / VU_COUNT;
+		var magnitude: float = aud_spectrum.get_magnitude_for_frequency_range(prev_hz, hz).length()
+		var energy = clamp((MIN_DB + linear2db(magnitude)) / MIN_DB, 0, 1)
+		var height = energy * HEIGHT
+		var center_ref = (res_w / 2) - (WIDTH / 2)
+		var rect_U = Rect2((w * i) + center_ref + 4, HEIGHT - height, w - 8, height)
+		var rect_D = Rect2((w * i) + center_ref + 4, HEIGHT - height, w - 8, height)
+		rect_U.position.y = rect_U.position.y + 85
+		rect_D.position.y = (720 / 2) + 25
+		draw_rect(rect_U, Color(1.0, 1.0, 1.0, 0.4))
+		draw_rect(rect_D, Color(1.0, 1.0, 1.0, 0.4))
+		prev_hz = hz
+
+func _process(_delta):
+	update()
+
+func _ready():
+	aud_spectrum = AudioServer.get_bus_effect_instance(0,0)
