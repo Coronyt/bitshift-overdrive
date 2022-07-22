7fbc490987c2f18b102f0c5c4e492744c07341ae
Interroblank <coronyt.apoelius@gmail.com>
2022-07-22 01:24:08 -0500

    Update ReleaseLabel.gd

--- scripts/interface/ReleaseLabel.gd
+++ scripts/interface/ReleaseLabel.gd
@@ -1,4 +1,5 @@
 extends RichTextLabel
 
 func _ready():
-	pass # TODO.
+	if self.get_parent().name == "ScoreCounter":
+		self.bbcode_text = "[wave amp=25 freq=1][center]" + "-- " + Active.chart_name + " --"
