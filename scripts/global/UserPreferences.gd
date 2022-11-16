9b37dc90dd6bd9da391ab2b5621fac25f7306acd
Coronyt <coronyt.apoelius@gmail.com>
2022-11-15 19:06:38 -0600

    Update UserPreferences.gd

--- scripts/global/UserPreferences.gd
+++ scripts/global/UserPreferences.gd
@@ -1,14 +1,26 @@
 extends Node
 
-const PREF_PATH = "res://userprefs.cfg"
+const PREF_PATH = "res://prefs.cfg"
 
-# []
+var pref_cfg = ConfigFile.new()
+var prefs = {
+	"disable_screen_curve":		false,
+	"disable_visualizer":		false,
+	"disable_scrolling":		false,
+	"disable_tooltips":			false,
+	"paddle_drag":				0.0,
+	"master_vol":				100.0,
+	"SFX_vol":					100.0,
+}
 
 func _ready():
-	pass
+	save_prefs()
+	load_prefs()
 
 func save_prefs():
-	pass
+	for pref_key in prefs.keys():
+		pref_cfg.set_value("UserPreferences", pref_key, prefs[pref_key])
+	pref_cfg.save(PREF_PATH)
 
 func load_prefs():
 	pass
