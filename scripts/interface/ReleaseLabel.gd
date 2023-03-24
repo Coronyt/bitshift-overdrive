b7326166d7d94ada83d1cfc3df70008e490190d6
Coronyt <coronyt.apoelius@gmail.com>
2023-03-24 18:09:57 -0500

    Dynamic ReleaseLabel.tscn

--- scripts/interface/ReleaseLabel.gd
+++ scripts/interface/ReleaseLabel.gd
@@ -1,5 +1,9 @@
 extends RichTextLabel
 
 func _ready():
+	if self.get_parent().name == "TitleScreen":
+		self.bbcode_text = "[wave amp=25 freq=1][center]BITSHIFT OVERDRIVE - PRE-RELEASE BUILD"
+	if self.get_parent().name == "LevelSelect":
+		self.bbcode_text = "[wave amp=25 freq=1][center]BITSHIFT OVERDRIVE - PRE-RELEASE BUILD"
 	if self.get_parent().name == "ScoreCounter":
 		self.bbcode_text = "[wave amp=25 freq=1][center]" + "-- " + Active.chart_name + " --"
