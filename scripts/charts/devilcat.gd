b890cc5bc12ca10ed159db6c349b7d0bff1bd624
Coronyt <coronyt.apoelius@gmail.com>
2023-08-13 23:19:21 -0500

    Started charting [cat-eared devil]

--- scripts/charts/devilcat.gd
+++ scripts/charts/devilcat.gd
@@ -27,16 +27,109 @@ const chart = [
 	# Buffer
 	["REST", 1],
 	["REST", 1],
-	
-	["REST", 1],
-	["REST", 1],
 	["REST", 1],
 	["REST", 1],
 	
-	["REST", 1],
-	["REST", 1],
-	["REST", 1],
-	["REST", 1],
+	# Offset
+	["REST", 16],
+	
+	# Measure 5
+	["NOTE", 4, AXIS_13],
+	["NOTE", 4, AXIS_10],
+	["NOTE", 16, AXIS_12],
+	["NOTE", 16, AXIS_12],
+	["REST", 16],
+	["NOTE", 16, AXIS_13],
+	["REST", 4],
+	
+	["NOTE", 8, AXIS_09],
+	["NOTE", 8, AXIS_10],
+	["NOTE", 8, AXIS_11],
+	["NOTE", 16, AXIS_10],
+	["NOTE", 16, AXIS_09],
+	
+	["NOTE", 16, AXIS_09],
+	["REST", 32],
+	["NOTE", 32, AXIS_08],
+	["NOTE", 32, AXIS_09],
+	["NOTE", 32, AXIS_08],
+	["NOTE", 16, AXIS_10],
+	
+	["NOTE", 16, AXIS_11],
+	["NOTE", 8, AXIS_07],
+	["NOTE", 16, AXIS_08],
+	
+	# Measure 7
+	["NOTE", 4, AXIS_13],
+	["NOTE", 8, AXIS_10],
+	["REST", 16],
+	["NOTE", 16, AXIS_12],
+	["NOTE", 16, AXIS_12],
+	["NOTE", 16, AXIS_12],
+	["REST", 16],
+	["NOTE", 16, AXIS_13],
+	["REST", 4],
+	
+	["NOTE", 8, AXIS_12],
+	["NOTE", 8, AXIS_13],
+	["NOTE", 16, AXIS_14],
+	["NOTE", 8, AXIS_15],
+	["NOTE", 4, AXIS_14],
+	["REST", 16],
+	
+	["NOTE", 16, AXIS_17],
+	["NOTE", 16, AXIS_16],
+	["NOTE", 16, AXIS_15],
+	["NOTE", 16, AXIS_14],
+	
+	# Measure 9
+	["NOTE", 4, AXIS_13],
+	["NOTE", 4, AXIS_10],
+	["NOTE", 16, AXIS_12],
+	["NOTE", 16, AXIS_12],
+	["REST", 16],
+	["NOTE", 16, AXIS_13],
+	["REST", 4],
+	
+	["NOTE", 8, AXIS_09],
+	["NOTE", 8, AXIS_10],
+	["NOTE", 8, AXIS_11],
+	["NOTE", 16, AXIS_10],
+	["NOTE", 16, AXIS_09],
+	
+	["NOTE", 16, AXIS_09],
+	["REST", 32],
+	["NOTE", 32, AXIS_08],
+	["NOTE", 32, AXIS_09],
+	["NOTE", 32, AXIS_08],
+	["NOTE", 16, AXIS_10],
+	
+	["NOTE", 16, AXIS_11],
+	["NOTE", 8, AXIS_07],
+	["NOTE", 16, AXIS_08],
+	
+	# Measure 11
+	["NOTE", 4, AXIS_13],
+	["NOTE", 8, AXIS_10],
+	["REST", 16],
+	["NOTE", 16, AXIS_12],
+	["NOTE", 16, AXIS_12],
+	["NOTE", 16, AXIS_12],
+	["REST", 16],
+	["NOTE", 16, AXIS_13],
+	["REST", 4],
+	
+	["NOTE", 8, AXIS_12],
+	["NOTE", 8, AXIS_13],
+	["NOTE", 16, AXIS_14],
+	["NOTE", 8, AXIS_15],
+	["NOTE", 4, AXIS_14],
+	["REST", 16],
+	
+	["NOTE", 16, AXIS_17],
+	["NOTE", 16, AXIS_16],
+	["NOTE", 16, AXIS_15],
+	["NOTE", 16, AXIS_14],
 	
 	# Buffer
 	["REST", 1],
