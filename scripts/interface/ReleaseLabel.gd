2ffc070366c48a6011b3981f032ee53c90671600
Coronyt <coronyt.apoelius@gmail.com>
2023-12-04 00:23:58 -0600

    Finished charting Scarlet

--- scripts/interface/ReleaseLabel.gd
+++ scripts/interface/ReleaseLabel.gd
@@ -2,8 +2,8 @@ extends RichTextLabel
 
 func _ready():
 	if self.get_parent().name == "TitleScreen":
-		self.bbcode_text = "[wave amp=25 freq=1][center]BITSHIFT OVERDRIVE - RELEASE BUILD (1.0.0) - THANK YOU FOR PLAYING!"
+		self.bbcode_text = "[wave amp=25 freq=1][center]BITSHIFT OVERDRIVE - MADE IN GODOT ENGINE 3 - THANKS FOR PLAYING!"
 	if self.get_parent().name == "LevelSelect":
-		self.bbcode_text = "[wave amp=25 freq=1][center]BITSHIFT OVERDRIVE - RELEASE BUILD (1.0.0) - THANK YOU FOR PLAYING!"
+		self.bbcode_text = "[wave amp=25 freq=1][center]BITSHIFT OVERDRIVE - MADE IN GODOT ENGINE 3 - THANKS FOR PLAYING!"
 	if self.get_parent().name == "ScoreCounter":
 		self.bbcode_text = "[wave amp=25 freq=1][center]" + "-- " + Active.chart_name + " --"
