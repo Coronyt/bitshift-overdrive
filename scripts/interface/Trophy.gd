20a19927ab7f1e2a0bff7166d5e828060bc09b0e
Coronyt <coronyt.apoelius@gmail.com>
2023-12-09 05:26:27 -0600

    Added shine FX to trophies

---
+++ scripts/interface/Trophy.gd
@@ -0,0 +1,18 @@
+extends Sprite
+
+func _ready():
+	$TrophyShader/SSShinyFx._set_active(false)
+
+func _on_TrophyTimer1_timeout():
+	var test = self.texture.resource_path 
+	print(self.name + " resource path: " + test)
+	if (self.texture.resource_path != "res://assets/sprites/trophy_0.png") and (
+		self.texture.resource_path != "res://assets/sprites/trophy_0.stex"):
+		$TrophyShader/SSShinyFx.play(0.5)
+	$TrophyTimer2.start()
+
+func _on_TrophyTimer2_timeout():
+	if (self.texture.resource_path != "res://assets/sprites/trophy_0.png") and (
+		self.texture.resource_path != "res://assets/sprites/trophy_0.stex"):
+		$TrophyShader/SSShinyFx._set_active(false)
+	$TrophyTimer1.start()
