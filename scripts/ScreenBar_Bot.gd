e50e0a731d376a20e273e321b994eb0fa526f41e
Coronyt <coronyt.apoelius@gmail.com>
2024-02-24 22:47:50 -0600

    Refactored ScreenBar entities for Coyglance
    Removed physics_process() functions, created scenes for ScreenBar_Top and ScreenBar_Bot (only for Coyglance currently, need to update other charts)

--- scripts/ScreenBar_Bot.gd
+++ scripts/ScreenBar_Bot.gd
@@ -1,12 +1,13 @@
 extends Area2D
 
-func _physics_process(_delta):
-	for coll in self.get_overlapping_bodies():
-		if not coll.shattered:
-			SoundManager.play_sound("miss")
-			if Active.is_byte or Active.is_nybl:
-				Active.health -= 1
-			self.get_parent().get_parent().get_parent().check_health()
-			Active.combo = 0
-			Active.last_milestone = 0
-			coll.queue_free()
+func _on_ScreenBar_Bot_body_entered(coll):
+	if coll:
+		if coll.get_child(0).name == "NoteSprite":
+			if not coll.shattered:
+				SoundManager.play_sound("miss")
+				if Active.is_byte or Active.is_nybl:
+					Active.health -= 1
+				self.get_parent().get_parent().get_parent().check_health()
+				Active.combo = 0
+				Active.last_milestone = 0
+				coll.queue_free()
