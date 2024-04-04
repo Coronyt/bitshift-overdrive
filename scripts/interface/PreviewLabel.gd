0b8d420aef8e05d62c678ce762ad32e205e771cf
Coronyt <coronyt.apoelius@gmail.com>
2024-04-04 03:54:16 -0500

    Removed _process() func from PreviewLabel.gd

--- scripts/interface/PreviewLabel.gd
+++ scripts/interface/PreviewLabel.gd
@@ -1,6 +1,6 @@
 extends RichTextLabel
 
-func _physics_process(_delta):
+func _on_new_active_track():
 	if Active.chart != "":
 		var to_bb = "[wave amp=25 freq=1][center]" + "-- " + Active.chart_name + " --"
 		if self.bbcode_text != to_bb:
