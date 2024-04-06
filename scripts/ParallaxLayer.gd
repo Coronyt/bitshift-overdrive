ab38d723c9191c341f84673c1afca0c425dc6865
Coronyt <coronyt.apoelius@gmail.com>
2024-04-06 03:35:28 -0500

    Added new BG to Coyglance

--- scripts/ParallaxLayer.gd
+++ scripts/ParallaxLayer.gd
@@ -7,5 +7,5 @@ export(int) var orig_y = 0
 func _physics_process(_delta):
 	if not UserPreferences.prefs["disable_scrolling"]:
 		self.translate(Vector2(-speed, 0))
-		if(self.global_transform.origin.x <= -1440):
+		if(self.global_transform.origin.x <= -640):
 			self.global_transform.origin = Vector2(orig_x, orig_y)
