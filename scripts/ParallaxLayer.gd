cb6366e9d1cb94fbf7efbecec6f216ff84fc29e4
Coronyt <coronyt.apoelius@gmail.com>
2024-04-07 05:33:18 -0500

    Added Devilcat BG

--- scripts/ParallaxLayer.gd
+++ scripts/ParallaxLayer.gd
@@ -3,9 +3,10 @@ extends Sprite
 export(float) var speed = 0
 export(int) var orig_x = 0
 export(int) var orig_y = 0
+export(bool) var one_shot
 
 func _physics_process(_delta):
 	if not UserPreferences.prefs["disable_scrolling"]:
 		self.translate(Vector2(-speed, 0))
-		if(self.global_transform.origin.x <= -640):
+		if(self.global_transform.origin.x <= -640) and not one_shot:
 			self.global_transform.origin = Vector2(orig_x, orig_y)
