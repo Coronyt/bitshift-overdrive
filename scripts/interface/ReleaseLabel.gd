5ecfa89635649d18cfb46ad2198b5c9acbe3fe81
Coronyt <coronyt.apoelius@gmail.com>
2024-04-07 09:39:22 -0500

    Updated ReleaseLabel.gd

--- scripts/interface/ReleaseLabel.gd
+++ scripts/interface/ReleaseLabel.gd
@@ -2,8 +2,8 @@ extends RichTextLabel
 
 func _ready():
 	if self.get_parent().name == "TitleScreen":
-		self.bbcode_text = "[wave amp=25 freq=1][center]BITSHIFT OVERDRIVE - MADE WITH GODOT ENGINE 3 - THANK YOU FOR PLAYING!"
+		self.bbcode_text = "[wave amp=25 freq=1][center]BITSHIFT OVERDRIVE 1.1 - MADE WITH GODOT ENGINE 3 - THANK YOU FOR PLAYING!"
 	if self.get_parent().name == "LevelSelect":
-		self.bbcode_text = "[wave amp=25 freq=1][center]BITSHIFT OVERDRIVE - MADE WITH GODOT ENGINE 3 - THANK YOU FOR PLAYING!"
+		self.bbcode_text = "[wave amp=25 freq=1][center]BITSHIFT OVERDRIVE 1.1 - MADE WITH GODOT ENGINE 3 - THANK YOU FOR PLAYING!"
 	if self.get_parent().name == "ScoreCounter":
 		self.bbcode_text = "[wave amp=25 freq=1][center]" + "-- " + Active.chart_name + " --"
